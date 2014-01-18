2eba00efc8e061533ab6e824d2b16dcf353f82cd
Lukas Welte <lukas.welte@googlemail.com>
2014-01-18 14:37:22 +0100

    Added ion to gradle

---
+++ android/Projects/Meetup/src/main/java/com/adm/meetup/helpers/NetworkHelper.java
@@ -0,0 +1,12 @@
+package com.adm.meetup.helpers;
+
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+import com.koushikdutta.ion.Response;
+
+/**
+ * Created by lukas on 18.01.14.
+ */
+public class NetworkHelper {
+    
+}
