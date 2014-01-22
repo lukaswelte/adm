56b5cde57382088a5b751a11d36187b61b89adf5
FlorianGz <florian.ganzin@gmail.com>
2014-01-22 11:56:48 +0100

    ActionBar event list

--- android/Projects/Meetup/src/main/java/com/adm/meetup/EventDescriptionFragment.java
+++ android/Projects/Meetup/src/main/java/com/adm/meetup/EventDescriptionFragment.java
@@ -8,25 +8,15 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ListView;
 import android.widget.Toast;
-
-import com.adm.meetup.event.Event;
 import com.adm.meetup.event.EventManager;
 
-import java.util.Date;
 import java.util.List;
 
-import static junit.framework.Assert.assertNotSame;
-
 /**
- * Created by lukas on 21.01.14.
+ * Created by florian on 22/01/2014.
  */
-public class EventListFragment extends Fragment {
-
-    private ListView eventListView;
-
+public class EventDescriptionFragment  extends Fragment  {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_event_list, container, false);
@@ -36,33 +26,7 @@ public class EventListFragment extends Fragment {
     public void onStart() {
         super.onStart();
 
-        eventListView = (ListView) getView().findViewById(R.id.eventListView);
         EventManager manager = new EventManager(getActivity());
-        List<Event> eventList = manager.getEvents();
-
-        /* Test on event */
-        manager.deleteEvents();
-        Event event = new Event();
-        event.setId(Long.valueOf(1));
-        event.setName("Event Name");
-        event.setLocation("Paris");
-        event.setAttendee((long)231);
-        manager.createEvent(event);
-        eventList.add(event);
-
-        EventListAdapter eventListAdapter = new EventListAdapter(getActivity(), eventList);
-        eventListView.setAdapter(eventListAdapter);
-
-
-        eventListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                Toast.makeText(getActivity().getApplicationContext(), "You Clicked at event " + position, Toast.LENGTH_SHORT).show();
-
-            }
-        });
     }
 
 
