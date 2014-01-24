a4347f86a4420b6bfcfd8197295293f9747fd420
Timo Mueller <timo.h.mueller@gmx.de>
2014-01-24 13:17:39 +0100

    final fixes

--- android/Projects/Meetup/src/main/java/com/adm/meetup/FriendsFragment.java
+++ android/Projects/Meetup/src/main/java/com/adm/meetup/FriendsFragment.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.TextView;
 
 import com.adm.meetup.User.User;
 import com.adm.meetup.helpers.NetworkHelper;
@@ -75,9 +74,11 @@ public class FriendsFragment extends Fragment {
                             }
                         });
                     }
+                    /*
                     TextView tv = (TextView) getView().findViewById(R.id.no_friends);
                     if (listItem.isEmpty()) tv.setVisibility(View.VISIBLE);
                     else tv.setVisibility(View.GONE);
+                    */
                 }
             }
         });
